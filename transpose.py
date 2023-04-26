761cb636512c2fd4e2f6ab57eb0e3e428a8d927c
Jonathan McGrath <mcgrath.jon@gmail.com>
2023-04-26 12:16:13 +0000

    Add transpose TCX function with ability to save TCX file

--- transpose.py
+++ transpose.py
@@ -1,4 +1,5 @@
 from garmin_fit_sdk import Decoder, Stream
+import xml.etree.cElementTree as ET
 
 from os.path import exists
 
@@ -18,20 +19,56 @@ def transpose_FIT(fit_stream, transposed_starting_lat, transposed_starting_lon):
     transposed_coords = []
     decoder = Decoder(fit_stream)
     messages, errors = decoder.read()
-    starting_lat = None
-    starting_lon = None
+    delta_lat = None
+    delta_lon = None
 
     for message in messages["record_mesgs"]:
         lat = message["position_lat"]*semicircle_conversion
         lon = message["position_long"]*semicircle_conversion
         alt = message["enhanced_altitude"]
 
-        if starting_lat is None:
-            starting_lat = lat
-            starting_lon = lon
+        if delta_lat is None:
+            delta_lat = lat - transposed_starting_lat
+            delta_lon = lon - transposed_starting_lon
 
-        transposed_lon = (lon-starting_lon)+transposed_starting_lon
-        transposed_lat = (lat-starting_lat)+transposed_starting_lat
+        transposed_lon = lon - delta_lon
+        transposed_lat = lat - delta_lat
         transposed_coords.append([transposed_lon, transposed_lat, alt])
 
     return transposed_coords
+
+def transpose_TCX(TCX_filename, transposed_starting_lat, transposed_starting_lon, save_file_overwrite=False,new_filename=None,return_format="xml"):
+
+    tree = ET.parse(TCX_filename)
+    root = tree.getroot()
+
+    transposed_coords = []
+    delta_lat = None
+    delta_lon = None
+
+    for trackpoint in root.iter("{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}Trackpoint"):
+        position = trackpoint.find('{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}Position')
+        lat = float(position.find('{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}LatitudeDegrees').text)
+        lon = float(position.find('{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}LongitudeDegrees').text)
+        alt = float(trackpoint.find('{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}AltitudeMeters').text)
+
+        if delta_lat is None:
+            delta_lat = lat - transposed_starting_lat
+            delta_lon = lon - transposed_starting_lon
+
+        transposed_lon = lon - delta_lon
+        transposed_lat = lat - delta_lat
+        position.find('{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}LatitudeDegrees').text=str(transposed_lat)
+        position.find('{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}LongitudeDegrees').text=str(transposed_lon)
+        transposed_coords.append([transposed_lon, transposed_lat, alt])
+
+    if save_file_overwrite:
+        if new_filename is not None:
+            tree.write(new_filename)
+        else:
+            tree.write(TCX_filename)
+
+    if return_format=="list":
+        return transposed_coords
+    else:
+        return tree
